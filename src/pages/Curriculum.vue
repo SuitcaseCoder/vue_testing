a5b1958248bd1f37380a109145ad810988b2d383
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-11-03 15:57:29 -0500

    SHA from specific branch for PUT request

--- src/pages/Curriculum.vue
+++ src/pages/Curriculum.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="curriculum-container">
         <!-- <Response :content="this.download_url" /> -->
-        <Button @btn-click="editContent(this.fileName)" text="edit" />
+        <Button @btn-click="editContent()" text="edit" />
         <article v-html="compiledHtml"></article>
 
     </div>    
@@ -26,10 +26,12 @@ export default {
     },
     data(){
         return {
+            getData: {},
             download_url: "",
             fileName: "",
             input: "",
             updatedContent: {},
+            editSHA: ""
             
         }
     },    computed: {
@@ -38,22 +40,18 @@ export default {
         }
       },
     methods: {
+        
         async fetchCurriculum(){
-            // https://github.com/SuitcaseCoder/sample-content/blob/main/js-1/one.md
-            // 'GET /repos/{owner}/{repo}/contents/{path}'
+            console.log("ON PAGE LOAD")
             const res = await octokit.request('GET /repos/{owner}/{repo}/contents/js-1/{path}', {
             owner: 'SuitcaseCoder',
             repo: 'sample-content',
             path: 'one.md',
             branch: 'platform_updates'
-
             })
             console.log(res);
+            console.log(res.data.sha);
             return res;
-            // console.log('fetched curriculum');
-            // const response = await axios.get(`api/repos/suitcaseCoder/sample-content/contents/js-1/one.md`)
-            // console.log(response.data.url);
-            // return response;
         },
         loadFile(){
                 axios({
@@ -69,32 +67,29 @@ export default {
         },
         async editContent(){
             const updatedContent = 
-            "### Title \n ## sub title \n ## update number: 4 \n new line over here some text blah blah blah blah \n ``` <h1> hello world </h1> ``` \n some more text over here blah blah oaisjekfjnifuanef"
- 
-            console.log("edit clicked");
-            // /repos/{owner}/{repo}/contents/js-1/{path}
+                "# update number: 6 \n ### Title \n ## sub title \n another sub title  \n new line over here some text blah blah blah blah \n ``` <h1> hello world </h1> ``` \n some more text over here blah blah oaisjekfjnifuanef"
+
+            // get SHA from branch we want to edit:
+            const branchToUpdate = await octokit.request('GET /repos/{owner}/{repo}/contents/js-1/{path}', {
+                owner: 'SuitcaseCoder',
+                repo: 'sample-content',
+                path: 'one.md',
+                branch: 'platform_updates',
+                // ref must reference the correct branch which from you want the SHA to then make a PUT request to
+                ref: 'platform_updates'
+            })
+            this.editSHA = branchToUpdate.data.sha;
+            console.log(this.editSHA);
+
             const res = await octokit.request('PUT /repos/{owner}/{repo}/contents/js-1/{path}', {
                 owner: 'suitcaseCoder',
                 repo: 'sample-content',
                 path: 'one.md',
                 message: 'message',
                 content: window.btoa(updatedContent),
-                sha: "fde6b903877d9f92713dcbba7ca5bbdec79d8cda",
+                sha: this.editSHA,
                 branch: 'platform_updates'
-            })
-            // const res = await octokit.request(`PUT  /repos/{owner}/{repo}/contents/js-1/{path}`, {
-            //     headers: {
-            //         Authorization: `token ${process.env.VUE_APP_GITHUB_API_KEY}`,
-            //         accept: "application/vnd.github.v3+json"
-            //     },
-            //    data: {
-            //         owner: 'suitcaseCoder',
-            //         repo: 'sample-content',
-            //         path: 'one.md',
-            //         message: 'message',
-            //         content: 'updated content'
-            //     }
-            // })  
+            }) 
             console.log(res);
             const updated = await res.json();
             console.log(updated);
@@ -103,6 +98,8 @@ export default {
     },
     async created (){
         const data = await this.fetchCurriculum();
+        this.editSHA = "";
+        this.data = data.data;
         this.download_url = data.data.download_url;
         this.fileName = data.data.name;
         this.loadFile();
